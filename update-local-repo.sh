5b53db80837e881df4373e3456701d5a94cfbf7a
Marek Marczykowski <marmarek@invisiblethingslab.com>
2011-10-12 15:21:53 +0200

    do not fail when starting for the first time

--- update-local-repo.sh
+++ update-local-repo.sh
@@ -6,6 +6,8 @@ REPO_DIR=$PWD/all-qubes-pkgs
 
 mkdir -p $REPO_DIR/rpm
 
-ln -f qubes-src/*/rpm/*/*.rpm $REPO_DIR/rpm/
+if ls qubes-src/*/rpm/*/*.rpm >/dev/null 2>&1; then
+    ln -f qubes-src/*/rpm/*/*.rpm $REPO_DIR/rpm/
+fi
 
 createrepo --update $REPO_DIR                         
