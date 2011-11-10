a7248de537059d34e6f2ae609a5fae7417eee749
Joanna Rutkowska <joanna@invisiblethingslab.com>
2011-11-10 18:47:46 +0100

    Properly handler NO_SIGN

--- build.sh
+++ build.sh
@@ -47,7 +47,7 @@ cp -alt $DIST_SRC_ROOT $ORIG_SRC
 rm -rf $DIST_SRC/rpm/{x86_64,i686,noarch}
 # Disable rpm signing in chroot - there are no signing keys
 sed -i -e 's/rpm --addsign/echo \0/' $DIST_SRC/Makefile*
-sudo chroot $DIST su - -c "cd /home/user/qubes-src/$COMPONENT; make $MAKE_TARGET" $RUN_AS_USER
+sudo -E chroot $DIST su - -c "cd /home/user/qubes-src/$COMPONENT; NO_SIGN="$NO_SIGN" make $MAKE_TARGET" $RUN_AS_USER
 for i in $DIST_SRC/rpm/*; do
     ARCH_RPM_DIR=$ORIG_SRC/rpm/`basename $i`
     mkdir -p $ARCH_RPM_DIR
