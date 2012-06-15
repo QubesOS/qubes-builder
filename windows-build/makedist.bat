5d296ac6a33444fb221b447c8e04cc4f641595d0
Marek Marczykowski <marmarek@invisiblethingslab.com>
2012-06-15 17:51:27 +0200

    windows: improve error handling v2
    Do not close cmd.exe on error, just skip next commands.

--- windows-build/makedist.bat
+++ windows-build/makedist.bat
@@ -1,4 +1,6 @@
-CMD /C "CD /D %CD%\winpvdrivers && makedist" || exit 1
-CMD /C "CD /D %CD%\core\win && makedist" || exit 1
+CMD /C "CD /D %CD%\winpvdrivers && makedist" || GOTO END
+CMD /C "CD /D %CD%\core\win && makedist" || GOTO END
 
-CMD /C "CD /D %CD% && wix" || exit 1
+CMD /C "CD /D %CD% && wix" || GOTO END
+
+:END
