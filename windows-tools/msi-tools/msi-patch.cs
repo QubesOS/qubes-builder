3efbc46055fb692e904cf56403469a6d701b3ad0
Rafał Wojdyła <omeg@omeg.pl>
2013-09-05 01:43:30 +0200

    Added preparing Windows build environment to the build process

---
+++ windows-tools/msi-tools/msi-patch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using WindowsInstaller;
+
+// This utility patches a MSI installer package to use new product GUID.
+// Reference msi.dll COM wrapper DLL when compiling.
+
+namespace Qubes.BuildTools
+{
+    static class MsiPatch
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: MsiPatch <msi path> <new GUID>");
+                return 2;
+            }
+			
+            try
+            {
+                string path = Path.GetFullPath(args[0]);
+                string guid = args[1];
+                Type t = Type.GetTypeFromProgID("WindowsInstaller.Installer");
+                Installer msi = (Installer) Activator.CreateInstance(t);
+                Database db = msi.OpenDatabase(path, MsiOpenDatabaseMode.msiOpenDatabaseModeTransact);
+                // update summary info
+                SummaryInfo summary = db.get_SummaryInformation(1);
+                summary.set_Property(9, guid);
+                summary.Persist();
+                // update property
+                string sql = string.Format("UPDATE Property SET Value='{0}' WHERE Property='ProductCode'", guid);
+                View view = db.OpenView(sql);
+                view.Execute(null);
+                view.Close();
+                sql = string.Format("UPDATE Property SET Value='{0}' WHERE Property='UpgradeCode'", guid);
+                view = db.OpenView(sql);
+                view.Execute(null);
+                view.Close();
+                db.Commit();
+                Marshal.ReleaseComObject(msi);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}\n{1}", e.Message, e.StackTrace);
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ windows-tools/msi-tools/tlb-convert.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+
+// This utility extracts COM type library definition from unmanaged DLL and creates matching managed wrapper assembly.
+
+namespace Qubes.BuildTools
+{
+    static class TlbConvert
+    {
+        private enum RegKind
+        {
+            RegKind_Default = 0,
+            RegKind_Register = 1,
+            RegKind_None = 2
+        }
+
+        [DllImport("oleaut32.dll", CharSet = CharSet.Unicode, PreserveSig = false)]
+        private static extern void LoadTypeLibEx(String strTypeLibName, RegKind regKind,
+            [MarshalAs(UnmanagedType.Interface)] out Object typeLib);
+
+        static int Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: TlbConvert <unmanaged COM DLL path> <managed wrapper path> [root namespace]");
+                return 2;
+            }
+
+            try
+            {
+                string srcDll = args[0];
+                string outDll = args[1];
+                string rootNamespace = args.Length == 3 ? args[2] : null;
+
+                Object typeLib;
+                LoadTypeLibEx(srcDll, RegKind.RegKind_None, out typeLib);
+                TypeLibConverter tlbConv = new TypeLibConverter();
+                AssemblyBuilder asm = tlbConv.ConvertTypeLibToAssembly(typeLib, outDll, 0, new ConversionEventHandler(), null, null, rootNamespace, null);
+                asm.Save(outDll);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}\n{1}", e.Message, e.StackTrace);
+                return 1;
+            }
+
+            Console.WriteLine("\nConversion successful.");
+            return 0;
+        }
+    }
+
+    public class ConversionEventHandler : ITypeLibImporterNotifySink
+    {
+        public void ReportEvent(ImporterEventKind eventKind, int eventCode, string eventMsg)
+        {
+            Console.WriteLine("{0}", eventMsg);
+        }
+
+        public Assembly ResolveRef(object typeLib)
+        {
+            return null;
+        }
+    }
+}
