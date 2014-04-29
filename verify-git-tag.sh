11ffc2d30cd86f33491755955a05896cb084494c
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2014-04-29 20:26:39 +0200

    verify-tag: check if tag is signed by trusted key, require one valid tag
    Unfortunately "git tag -v" isn't flexible enough, so reimplement if with
    simple bash script.

--- verify-git-tag.sh
+++ verify-git-tag.sh
@@ -22,13 +22,32 @@ else
 	REF="HEAD"
 fi
 
-TAG=`git tag --points-at=$REF | head -n 1`
+verify_tag() {
+	sig_header="-----BEGIN PGP SIGNATURE-----"
+	temp_name=`mktemp -d sig-verify.XXXXXX`
+	git cat-file tag $1 | sed "/$sig_header/,//d"  > $temp_name/content
+	git cat-file tag $1 | sed -n "/$sig_header/,//p" > $temp_name/content.asc
+	gpg --verify --status-fd=1 $temp_name/content.asc 2>/dev/null|grep -q '^\[GNUPG:\] TRUST_\(FULLY\|ULTIMATE\)$'
+	ret=$?
+	rm -r $temp_name
+	return $ret
+}
 
-if [ -z "$TAG" ]; then
-	echo "Source is not tagged, cannot verify it!"
+VALID_TAG_FOUND=0
+for tag in `git tag --points-at=$REF`; do
+	if verify_tag $tag; then
+		VALID_TAG_FOUND=1
+	else
+		if [ "0$VERBOSE" -ge 1 ]; then
+			echo "---> Invalid signature:"
+			git tag -v $tag
+		fi
+	fi
+done
+
+if [ "$VALID_TAG_FOUND" -eq 0 ]; then
+	echo "No valid signed tag found!"
 	exit 1
 fi
-
-git tag -v $TAG >/dev/null 2>/dev/null || { git tag -v $TAG; exit 1; }
 
 exit 0
