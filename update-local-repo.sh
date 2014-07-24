7cebdbbcdc529ac5bf6ee6b08f3050081e6bb46d
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2014-07-24 21:26:26 +0200

    debian: sign local repository
    Apparently apt have no way to configure one repository as not signed.

--- update-local-repo.sh
+++ update-local-repo.sh
@@ -5,11 +5,39 @@ set -e
 [ -z "$1" ] && { echo "Usage: $0 <dist>"; exit 1; }
 
 REPO_DIR=$PWD/qubes-rpms-mirror-repo/$1
+KEYS_DIR=$PWD/keys_debian
+DIST=$1
 
 if [ -d $REPO_DIR/dists ]; then
     pushd $REPO_DIR
-    mkdir -p dists/$1/main/binary-amd64
-    dpkg-scanpackages . |gzip > dists/$1/main/binary-amd64/Packages.gz
+    mkdir -p dists/$DIST/main/binary-amd64
+    dpkg-scanpackages . > dists/$DIST/main/binary-amd64/Packages
+    gzip -9c dists/$DIST/main/binary-amd64/Packages > dists/$DIST/main/binary-amd64/Packages.gz
+    cat > dists/$DIST/Release <<EOF
+Label: Qubes builder repo
+Suite: $DIST
+Codename: $DIST
+Date: `date -R`
+Architectures: amd64
+Components: main
+SHA1:
+EOF
+    function calc_sha1() {
+        f=dists/$DIST/$1
+        echo -n " "
+        echo -n `sha1sum $f|cut -d' ' -f 1` ""
+        echo -n `stat -c %s $f` ""
+        echo $1
+    }
+    calc_sha1 main/binary-amd64/Packages >> dists/$1/Release
+    calc_sha1 main/binary-amd64/Packages >> dists/$1/Release.gz
+
+    rm -f dists/$DIST/Release.gpg
+    gpg -abs --no-default-keyring \
+        --secret-keyring $KEYS_DIR/$1-secring.gpg \
+        --keyring $KEYS_DIR/$1-pubring.gpg \
+        -o dists/$DIST/Release.gpg \
+        dists/$DIST/Release
     popd
 else
     mkdir -p $REPO_DIR/rpm
