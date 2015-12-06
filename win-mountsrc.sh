4909063fb87e1cdd2f0c558165e17fec796a11ba
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-12-06 14:22:27 +0100

    windows: mount source image read-only by default
    Prevent error about unclean unmount.

--- win-mountsrc.sh
+++ win-mountsrc.sh
@@ -10,5 +10,5 @@ else
 OUTPUT=`sudo kpartx -s -a -v $SRCIMG`
 # sample output: add map loop0p1 (253:1): 0 2095104 linear /dev/loop0 2048
 DEV=/dev/mapper/`echo $OUTPUT | cut -f 3 -d ' '`
-sudo mount $DEV $MNT -o norecover,uid=`id -u`,fmask=133 || exit 1
+sudo mount $DEV $MNT -o norecover,uid=`id -u`,fmask=133,ro || exit 1
 fi
