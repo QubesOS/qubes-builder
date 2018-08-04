99104506f5c3d3edc0fcf9467c441f5215b08c71
Frédéric Pierret <frederic.epitre@orange.fr>
2018-08-04 18:51:36 +0200

    Allows to trig template build on Github inside the builder
    QubesOS/qubes-issues#3935

--- Makefile
+++ Makefile
@@ -347,6 +347,26 @@ template-local-%::
 		$(MAKE) -s -C $(SRC_DIR)/linux-template-builder $$MAKE_TARGET || exit 1; \
 	fi
 
+template-github: template-github.token $(DISTS_VM:%=template-github-%)
+
+template-github.token:
+	@if [ "x$(GITHUB_API_KEY)" != "x" ]; then \
+		echo "machine api.github.com login $(GITHUB_API_KEY) password x-oauth-basic" > $(BUILDER_DIR)/.netrc_github
+	else \
+		echo "Please provide GITHUB_API_KEY."; exit 1; \
+	fi
+
+template-github-%: DIST=$*
+template-github-%: GITHUB_API_FILE=$(BUILDER_DIR)/.netrc_github
+template-github-%:
+	@if [ "$(VERBOSE)" -eq 0 ]; then \
+		echo "-> Posting build command for template $$DIST (logfile: build-logs/template-github-$$DIST.log)..."; \
+		$(BUILDER_DIR)/scripts/generate_build_github.sh $(DIST) > build-logs/template-github-$$DIST.log 2>&1 || exit 1; \
+		echo "--> Done."; \
+	else \
+		$(BUILDER_DIR)/scripts/generate_build_github.sh $(DIST) || exit 1; \
+	fi;\
+
 template-in-dispvm: $(DISTS_VM:%=template-in-dispvm-%)
 
 template-in-dispvm-%: DIST=$*
---
+++ scripts/generate_build_github.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+[ "$DEBUG" = "1" ] && set -x
+
+# Defaulting to gpg2 if GNUPG is not set
+[[ "x$GNUPG" = "x" ]] && GNUPG=gpg2
+
+# Check if necessary variables are defined in the builder.conf
+for var in GITHUB_API_FILE GITHUB_BUILD_TARGET SIGN_KEY RELEASE
+do
+    if [ "x${!var}" = "x" ]; then
+        echo "Please provide $var in builder.conf"
+        exit 1
+    fi
+done
+
+if $GNUPG --list-secret-keys "$SIGN_KEY" &> /dev/null; then
+    # Create TIMESTAMP with respect to UTC
+    TIMESTAMP=$(date --utc +%Y%m%d%H%M)
+
+    # GITHUB URL to trigger a build
+    GITHUB_URL="https://api.github.com$GITHUB_BUILD_TARGET"
+
+    # Create build command and sign it. Replace newline with
+    # its corresponding character for JSON format
+    data=$(echo "Build-template r$RELEASE $DIST $TIMESTAMP" | $GNUPG --digest-algo SHA256 --clearsign -u $SIGN_KEY | sed ':a;N;$!ba;s/\n/\\n/g')
+
+    # POST json containing the signed build command
+    echo "{\"body\": \"$data\"}" | curl --netrc-file "$GITHUB_API_FILE" POST "$GITHUB_URL" --data-binary @-
+else
+    echo "Unable to find key $SIGN_KEY"; exit 1
+fi
+
